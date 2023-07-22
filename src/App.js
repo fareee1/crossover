92f236a5ea13320b738112bd59f361e35e723246
Shatuw <r.voigtmail@gmail.com>
2023-07-22 11:59:39 +0200

    added Searchbar with mock-Arrays

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import React, {useState} from 'react';
 import logo from "./images/yelp_logo.png";
 import { Searchbar } from "./components/Searchbar.js";
 import { Links } from "./components/Links.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  const [searchTag, setSearchTag] = useState();
+  const [searchCity, setSearchCity] = useState();
+
+  console.log(searchTag);
+  console.log(searchCity);
+  
   return (
     <div className="App">
       <div className="backgroundContainer">
         <div class="logoWrapper">
           <img src={logo} className="logo" alt="logo" />
         </div>
-        <Searchbar />
+        <Searchbar 
+          setSearchTag={setSearchTag} 
+          setSearchCity={setSearchCity}  
+        />
         <Links />
       </div>
     </div>
