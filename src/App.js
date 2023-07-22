9e4547a8e9f3034b6c9158054a754e4b88ede8dc
fareee1 <faris.bilalagic17@gmail.com>
2023-07-22 10:07:59 +0200

    added components, installed bootstrap and axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./images/yelp_logo.png";
+import {Searchbar} from "./components/Searchbar.js";
+import {Links} from "./components/Links.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img src={logo} alt="logo" />
+        <Searchbar />
+        <Links />
+      </div>
+      
     </div>
   );
 }
