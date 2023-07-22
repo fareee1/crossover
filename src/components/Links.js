0d8232d1795c8f6f942d13591842801166c71d99
fareee1 <faris.bilalagic17@gmail.com>
2023-07-22 12:21:40 +0200

    added links component and styling to it

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import {
+  MdRestaurant,
+  MdOutlineLocalBar,
+  MdOutlineBuild,
+  MdOutlineMoped,
+} from "react-icons/md";
 
 export const Links = () => {
   return (
-    <div>Links</div>
-  )
-}
+    <div>
+      <ul className="links">
+        <li>
+          <MdRestaurant className="icon" /><a href="#">Restaurants</a>
+        </li>
+        <li>
+          <MdOutlineLocalBar className="icon" /><a href="#">Nightlife</a>
+        </li>
+        <li>
+          <MdOutlineBuild className="icon" /><a href="#">Local Services</a>
+        </li>
+        <li>
+          <MdOutlineMoped className="icon" /><a href="#">Delivery</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export const Restaurants = () => {
-  return (
-    <div>Restaurants</div>
-  )
-}
+  return <div></div>;
+};
